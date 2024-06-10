150c2214d957eae415a5bdd4a08023b055e84d68
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-06-10 15:25:01 +0400

    scroll

--- src/pages/components/send-message.jsx
+++ src/pages/components/send-message.jsx
@@ -6,8 +6,7 @@ import "./message.css";
 const SendMessage = () => {
   const [message, setMessage] = useState("");
 
-  const sendMessage = async (event) => {
-    event.preventDefault();
+  const sendMessage = async () => {
     if (message.trim() === "") {
       alert("Enter valid message");
       return;
@@ -25,20 +24,27 @@ const SendMessage = () => {
 
   const handleInputChange = (event) => {
     setMessage(event.target.value);
-    
     event.target.style.height = "auto";
     event.target.style.height = event.target.scrollHeight + "px";
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage();
+    }
+  };
+
   return (
-    <form className="send-message-container" onSubmit={sendMessage}>
+    <form className="send-message-container" onSubmit={(e) => e.preventDefault()}>
       <textarea
         placeholder="Type your message"
         value={message}
         onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
         className="message-input"
       />
-      <button className="send-button">Send </button>
+      <button className="send-button" onClick={sendMessage}>Send</button>
     </form>
   );
 };
